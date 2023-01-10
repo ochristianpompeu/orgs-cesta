d3ca42b5ab7bf811a855c89f5a76aaf24ac6c78f
Christian <114309489+ochristianpompeu@users.noreply.github.com>
2023-01-10 19:13:02 -0300

    Criando tema customizado

---
+++ src/styles/theme.ts
@@ -0,0 +1,47 @@
+import { extendTheme } from "native-base";
+import {
+
+} from "@expo-google-fonts/montserrat";
+
+const theme = extendTheme({
+  fontConfig: {
+    Montserrat: {
+      100: {
+        normal: "Montserrat_100Thin",
+        italic: "Montserrat_100Thin_Italic",
+      },
+      200: {
+        normal: "Montserrat_200ExtraLight",
+        italic: "Montserrat_200ExtraLight_Italic",
+      },
+      300: {
+        normal: "Montserrat_300Light",
+        italic: "Montserrat_300Light_Italic",
+      },
+      400: {
+        normal: "Montserrat_400Regular",
+        italic: "Montserrat_400Regular_Italic",
+      },
+      500: {
+        normal: "Montserrat_500Medium",
+        italic: "Montserrat_500Medium_Italic",
+      },
+      600: {
+        normal: "Montserrat_600SemiBold",
+        italic: "Montserrat_600SemiBold_Italic",
+      },
+      700: {
+        normal: "Montserrat_700Bold",
+        italic: "Montserrat_700Bold_Italic",
+      },
+      800: {
+        normal: "Montserrat_800ExtraBold",
+        italic: "Montserrat_800ExtraBold_Italic",
+      },
+      900: {
+        normal: "Montserrat_900Black",
+        italic: "Montserrat_900Black_Italic",
+      },
+    },
+  },
+});
